570e413ab4f0d1098fe13351b0dd1bff79f2f420
BashfulCoder7 <codedemon1989@gmail.com>
2025-01-30 12:39:19 -0600

    Subtotal Card

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -17,6 +17,8 @@ import {
   TableCell
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { formatCurrency } from "@/lib/utils";
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter();
@@ -100,14 +102,33 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
                           )}
                         </Button>
                       </TableCell>
-                      <TableCell className="text-right">
-                        ${Number(item.price).toFixed(2)}
-                      </TableCell>
+                      <TableCell className="text-right">${Number(item.price).toFixed(2)}</TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
               </Table>
-            </div> 
+            </div>
+            
+            <Card>
+              <CardContent className="p-4 gap-4">
+                <div className="pb-3 text-xl">
+                  Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):{" "}
+                  <span className="font-bold">{formatCurrency(cart.itemsPrice)}</span>
+                </div>
+                <Button
+                  className="w-full"
+                  disabled={isPending}
+                  onClick={() => startTransition(() => router.push("/shipping-address"))}
+                >
+                  {isPending ? (
+                    <Loader className="w-4 h-4 animate-spin" />
+                  ) : (
+                    <ArrowRight className="w-4 h-4" />
+                  )}{" "}
+                  Proceed to Checkout
+                </Button>
+              </CardContent>
+            </Card>
         </div>
       )}
     </>
